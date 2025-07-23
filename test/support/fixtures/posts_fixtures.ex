4d9e5c2340e723b5bea9270d2b1bd11837cf63ae
Milton Isaya <miltonisaiah@gmail.com>
2025-07-23 12:46:54 +0300

    added test files

---
+++ test/support/fixtures/posts_fixtures.ex
@@ -0,0 +1,21 @@
+defmodule PhoenixBlog.PostsFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `PhoenixBlog.Posts` context.
+  """
+
+  @doc """
+  Generate a post.
+  """
+  def post_fixture(attrs \\ %{}) do
+    {:ok, post} =
+      attrs
+      |> Enum.into(%{
+        body: "some body",
+        title: "some title"
+      })
+      |> PhoenixBlog.Posts.create_post()
+
+    post
+  end
+end
